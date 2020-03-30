48d97681736c0ac077d7f234ae9c960ed329d12d
Grant Phelps <gbphelps2@gmail.com>
2020-03-30 09:02:01 -0400

    more work on cards

--- video.js
+++ video.js
@@ -1,6 +1,17 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     Array.from(document.getElementsByTagName('video')).forEach(vid => {
+//         vid.addEventListener('mouseenter', () => vid.play());
+//         vid.addEventListener('mouseleave', () => vid.pause());
+//     })
+// })
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    Array.from(document.getElementsByTagName('video')).forEach(vid => {
-        vid.addEventListener('mouseenter', () => vid.play());
-        vid.addEventListener('mouseleave', () => vid.pause());
+    const features = Array.from(document.getElementsByClassName('feature'));
+    features.forEach(f => {
+       f.addEventListener('click',()=>{
+           features.forEach(f => f.classList.remove('active'));
+           f.classList.toggle('active');
+       })
     })
 })
